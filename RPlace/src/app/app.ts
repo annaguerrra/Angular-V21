e1766aafdce1cb3907fa1939d9d97fed1bb2458a
annaguerrra <annaguerracastro@gmail.com>
2026-03-27 16:35:04 -0300

    date input

--- RPlace/src/app/app.ts
+++ RPlace/src/app/app.ts
@@ -1,14 +1,26 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    FormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('RPlace');
+  protected _date = signal(new Date());
+
+  protected _stringDate: Signal<string> = computed(() => {
+    return `${
+    this._date().getFullYear()}
+    -${this._date().getMonth()}
+    -${this._date().getDate()}`
+  });
   
   ngOnInit() {
     console.log(this.title()); // retorna o nome 'RPlace'
