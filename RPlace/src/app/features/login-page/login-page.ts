b0b0198f3ceb949efd0898162cb2ea5e8114e14e
annaguerrra <annaguerracastro@gmail.com>
2026-04-09 15:50:54 -0300

    login page

--- RPlace/src/app/features/login-page/login-page.ts
+++ RPlace/src/app/features/login-page/login-page.ts
@@ -1,12 +1,55 @@
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { LoginDto } from '../../domain/UserInterface';
+import { AuthApi } from '../../domain/auth.api';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+constructor( private api: AuthApi){}
 
+  loginForm : FormGroup = new FormGroup({
+    username: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  get Username() {
+    return this.loginForm.get('username')
+  }
+
+  get Password() {
+    return this.loginForm.get('password')
+  }
+
+  login = () => {
+    if(!this.loginForm.valid){
+      alert("Nem todos os campos foram preenchidos");
+      return
+    }
+
+    const data: LoginDto = {
+      username: this.Username?.value,
+      password: this.Password?.value,
+    }
+
+    this.api.login(data).subscribe(
+      res => {
+        console.log(res)
+        sessionStorage.setItem("token", res);
+        location.reload();
+      }
+    );
+  }
+
+  subscribe = () => {
+    if(!this.loginForm.valid){
+      alert("Nem todos os campos foram preenchidos");
+      return
+    }
+  }
   
 }
--- RPlace/src/app/features/main-page/main-page.ts
+++ RPlace/src/app/features/main-page/main-page.ts
@@ -1,9 +1,7 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Pixel } from './pixel/pixel';
 import { NavBar } from "../../shared/nav-bar/nav-bar";
-import { LoginDto } from '../../domain/UserInterface';
-import { AuthApi } from '../../domain/auth.api';
+
 
 @Component({
   selector: 'app-main-page',
@@ -15,46 +13,5 @@ export class MainPage {
   Width: number[] = new Array(30)
   Height: number[] = new Array(30)
   
-  constructor( private api: AuthApi){}
-
-  loginForm : FormGroup = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required])
-  })
-
-  get Username() {
-    return this.loginForm.get('username')
-  }
-
-  get Password() {
-    return this.loginForm.get('password')
-  }
-
-  login = () => {
-    if(!this.loginForm.valid){
-      alert("Nem todos os campos foram preenchidos");
-      return
-    }
-
-    const data: LoginDto = {
-      username: this.Username?.value,
-      password: this.Password?.value,
-    }
-
-    this.api.login(data).subscribe(
-      res => {
-        console.log(res)
-        sessionStorage.setItem("token", res);
-        location.reload();
-      }
-    );
-  }
-
-  subscribe = () => {
-    if(!this.loginForm.valid){
-      alert("Nem todos os campos foram preenchidos");
-      return
-  }
-
-  }
+  
 }
