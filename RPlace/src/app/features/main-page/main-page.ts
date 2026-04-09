0baf9e27ad7e21d7302a98a7a1d15271cb039bd7
annaguerrra <annaguerracastro@gmail.com>
2026-04-09 15:41:51 -0300

    configurando dto, storage etc

--- RPlace/src/app/features/main-page/main-page.ts
+++ RPlace/src/app/features/main-page/main-page.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Pixel } from './pixel/pixel';
 import { NavBar } from "../../shared/nav-bar/nav-bar";
+import { LoginDto } from '../../domain/UserInterface';
+import { AuthApi } from '../../domain/auth.api';
 
 @Component({
   selector: 'app-main-page',
@@ -10,7 +12,9 @@ import { NavBar } from "../../shared/nav-bar/nav-bar";
   styleUrl: './main-page.css',
 })
 export class MainPage {
-
+  Width: number[] = new Array(30)
+  Height: number[] = new Array(30)
+  
   constructor( private api: AuthApi){}
 
   loginForm : FormGroup = new FormGroup({
@@ -27,14 +31,30 @@ export class MainPage {
   }
 
   login = () => {
-    
+    if(!this.loginForm.valid){
+      alert("Nem todos os campos foram preenchidos");
+      return
+    }
+
+    const data: LoginDto = {
+      username: this.Username?.value,
+      password: this.Password?.value,
+    }
+
+    this.api.login(data).subscribe(
+      res => {
+        console.log(res)
+        sessionStorage.setItem("token", res);
+        location.reload();
+      }
+    );
   }
 
   subscribe = () => {
+    if(!this.loginForm.valid){
+      alert("Nem todos os campos foram preenchidos");
+      return
+  }
 
   }
-  
-  Width: number[] = new Array(30)
-  Height: number[] = new Array(30)
-
 }
